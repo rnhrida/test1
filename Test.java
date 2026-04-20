d888c506bc5841f3b95dffdd362e233116df4065
awa33 <66191215+awa33@users.noreply.github.com>
2026-04-20 10:04:47 +0530

    Add basic arithmetic operations in Test class- commit1

---
+++ Test.java
@@ -0,0 +1,45 @@
+public class Test {
+
+  static void add(){
+
+    int a=10;
+    int b=20;
+    int result = a+b;
+    System.out.println("The sum is " +result);
+    
+  }
+  static void sub(){
+
+    int c=20;
+    int d=10;
+    int result1 = c-d;
+    System.out.println("The diff is " +result1);
+    
+  }
+
+   static void multi(){
+
+    int x=5;
+    int y=8;
+    int result2 = x*y;
+    System.out.println("The multiplcation result is: " +result2);
+    
+  }
+
+  static void div(){
+
+    int s=10;
+    int t=5;
+    int result3 = s/t;
+    System.out.println("The division result is: " +result3);
+    
+  }
+
+  public static void main(String[] args)
+  {
+    add();
+    sub();
+    multi();
+    div();
+  }
+}
